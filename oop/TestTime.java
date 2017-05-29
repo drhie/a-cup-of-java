4f18ed58127dfc25e228b0938f47c45e0fe32092
drhie <rhie.david@gmail.com>
2017-05-28 20:09:27 -0400

    Introduce try + catch clauses in test

--- oop/TestTime.java
+++ oop/TestTime.java
@@ -12,16 +12,26 @@ public class TestTime {
     Time t1 = new Time();
     Time t2 = new Time(59, 32, 5);
 
-    test.testStates(t2);
-    test.testStates(t1);
-    t1.setSecond(62);
-    t1.setTime(88, 88, 88);
+    try {
+      test.testStates(t2);
+      test.testStates(t1);
+      t1.setSecond(62);
+      t2.setSecond(33); //Skipped
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    }
+    try {
+      t1.setTime(88, 88, 88);
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    }
     t1.setSecond(58);
     t1.setMinute(59);
     t1.setHour(20);
     test.testStates(t1);
     t1.nextSecond().nextSecond();
     test.testStates(t1);
+    test.testStates(t2);
   }
 
 }
